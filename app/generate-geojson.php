592801da8a5ca54949d34daa69382e95e9aa50a7
pax <pax@mioritics.ro>
2018-10-07 03:20:31 +0300

    generate aggregate structured data

--- app/generate-geojson.php
+++
@@ -1,9 +0,0 @@
-<style>body{font-family: sans-serif; padding: 5%;}.print_r{font-size: 12px;}</style>
-<?php
-$rustart = getrusage();
-require('functions.php');
-
-
-
-$ru = getrusage();
-echo '<p><small><b>'.rutime($ru, $rustart, "utime") . "</b> ms computations;\n<b>". rutime($ru, $rustart, "stime") . "</b> ms sys calls</small></p>\n";
