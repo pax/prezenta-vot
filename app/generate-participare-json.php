6d7f114b7f21abb6698e87608cde8c082e572565
pax <pax@mioritics.ro>
2018-10-07 13:30:40 +0300

    fix all sections under siruta

--- app/generate-participare-json.php
+++ app/generate-participare-json.php
@@ -22,16 +22,14 @@ foreach ($csv as $timestamp => $oneCsv) {
   // $masterData[$timestamp]=[];
   foreach ($oneCsv as $rowno => $row) {
     // pr($row);
-    $masterData[$row['Siruta']]['Nr sectie de votare']['siruta']=$row['Siruta'];
-    $masterData[$row['Siruta']]['Nr sectie de votare']['nr sectie']=$row['Nr sectie de votare'];
-    $masterData[$row['Siruta']]['Nr sectie de votare']['nume sectie']=$row['Nume sectie de votare'];
-    $masterData[$row['Siruta']]['Nr sectie de votare']['localitate']=$row['Localitate'];
-    $masterData[$row['Siruta']]['Nr sectie de votare']['county']=$row['﻿Judet'];
-    $masterData[$row['Siruta']]['Nr sectie de votare']['pe lista']=$row['Votanti lista'];
-    $masterData[$row['Siruta']]['Nr sectie de votare']['ts'][$timestamp]['LP']=$row['LP'];
-    $masterData[$row['Siruta']]['Nr sectie de votare']['ts'][$timestamp]['LS']=$row['LS'];
-    $masterData[$row['Siruta']]['Nr sectie de votare']['ts'][$timestamp]['UM']=$row['UM'];
-    $masterData[$row['Siruta']]['Nr sectie de votare']['ts'][$timestamp]['LT']=$row['LT'];
+    $masterData[$row['Siruta']][$row['Nr sectie de votare']]['nume sectie']=$row['Nume sectie de votare'];
+    $masterData[$row['Siruta']][$row['Nr sectie de votare']]['localitate']=$row['Localitate'];
+    $masterData[$row['Siruta']][$row['Nr sectie de votare']]['county']=$row['﻿Judet'];
+    $masterData[$row['Siruta']][$row['Nr sectie de votare']]['pe lista']=$row['Votanti lista'];
+    $masterData[$row['Siruta']][$row['Nr sectie de votare']]['ts'][$timestamp]['LP']=$row['LP'];
+    $masterData[$row['Siruta']][$row['Nr sectie de votare']]['ts'][$timestamp]['LS']=$row['LS'];
+    $masterData[$row['Siruta']][$row['Nr sectie de votare']]['ts'][$timestamp]['UM']=$row['UM'];
+    $masterData[$row['Siruta']][$row['Nr sectie de votare']]['ts'][$timestamp]['LT']=$row['LT'];
   }
 }
 
